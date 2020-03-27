297683d0c61ffc6049c43f67d78522cbc99d2a57
user <user@user-PC>
2020-03-27 17:58:16 +0900

    jsp_0327

---
+++ jsp_0327/WebContent/js/custom.js
@@ -0,0 +1,58 @@
+function f1(){
+	$("#puppy").animate({
+		"left":"600px",
+		"border-radius":"150px"
+	});
+}
+
+function f2(){
+	$("#cat").animate({
+		"left":"300px",
+		"border-radius":"30px",
+		"opacity":"50%"
+	});
+}
+
+
+$(document).ready(function(){
+
+	
+	
+//------------------------------------------------------------	
+	
+//	$("#panel").hide();
+//	
+//	$("#slide").click(function(){
+//		$("#panel").slideDown(2000);
+//	});
+//	
+//	$("#panel").click(function(){
+//		$("#panel").slideUp(2000);
+//	});
+//	
+//	$("#flip").click(function(){
+//		$("#panel").slideToggle(2000);
+//	});
+//	
+//	$("#stop").click(function(){
+//		$("#panel").stop();
+//	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				
